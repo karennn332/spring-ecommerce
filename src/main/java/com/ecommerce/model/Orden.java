e5fd82003b94c20587d9249b6c4b12c67512c032
karen <karennn332@gmail.com>
2023-07-13 15:39:46 -0300

    creacion de la estructura del proyecto y las clases model

--- src/main/java/com/ecommerce/model/Orden.java
+++ src/main/java/com/ecommerce/model/Orden.java
@@ -7,7 +7,68 @@ public class Orden {
 	private String numero;
 	private Date fechaCreacion;
 	private Date echaRecibida;
-	
 	private Double total;
+	
+	public Orden() {
+	}
 
+	public Orden(Integer id, String numero, Date fechaCreacion, Date echaRecibida, Double total) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.fechaCreacion = fechaCreacion;
+		this.echaRecibida = echaRecibida;
+		this.total = total;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Date getEchaRecibida() {
+		return echaRecibida;
+	}
+
+	public void setEchaRecibida(Date echaRecibida) {
+		this.echaRecibida = echaRecibida;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	@Override
+	public String toString() {
+		return "Orden [id=" + id + ", numero=" + numero + ", fechaCreacion=" + fechaCreacion + ", echaRecibida="
+				+ echaRecibida + ", total=" + total + "]";
+	}
+	
+	
+	
+  
+	
 }
