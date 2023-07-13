5236e56685aa3f52ff54851831854e9f37fefe5f
karen <karennn332@gmail.com>
2023-07-13 14:12:56 -0300

    primer commit
    Estructura del proyecto y clases

---
+++ src/main/java/com/ecommerce/model/Orden.java
@@ -0,0 +1,13 @@
+package com.ecommerce.model;
+
+import java.util.Date;
+
+public class Orden {
+	private Integer id;
+	private String numero;
+	private Date fechaCreacion;
+	private Date echaRecibida;
+	
+	private Double total;
+
+}
