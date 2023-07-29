4675d843fa484a920caf7bd094997dda3b8f31a7
karen <karennn332@gmail.com>
2023-07-28 21:18:51 -0300

    obtener usuario login y session

--- src/main/java/com/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.model.Usuario;
 import com.ecommerce.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/usuario")
@@ -34,4 +38,32 @@ public class UsuarioController {
 		 return "redirect:/";
 	 }
 	 
+	 @GetMapping("/login")
+	 public String login() {
+		 return "usuario/login";
+	 }
+	 
+	 
+	  // autenticacion manual para obtener el id del usuario y guardarlo en un seccion para poderlo llevar
+	 //a todos los lugares de controladores que necesiten
+	 @PostMapping("/acceder")
+	 public String acceder(Usuario usuario, HttpSession session) {
+		 logger.info("accesos : {}", usuario);
+		 
+		 Optional<Usuario> user=usuarioService.findByEmail(usuario.getEmail());
+		 //logger.info("Usuario de db; {}", user.get());
+		 
+		 if(user.isPresent()) {
+			 session.setAttribute("idusuario", user.get().getId());
+			 if (user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/administrador";
+			}else {
+				return "redirect:/";
+			}
+		} else {
+			logger.info("usuario no existe");
+		}
+		 	
+		 return "redirect:/";
+	 } 
 }
--- src/main/java/com/ecommerce/repository/IUsuarioRepository.java
+++ src/main/java/com/ecommerce/repository/IUsuarioRepository.java
@@ -1,10 +1,15 @@
 package com.ecommerce.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import com.ecommerce.model.Usuario;
+import java.util.List;
+
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
 
+	Optional<Usuario> findByEmail(String email);
 }
